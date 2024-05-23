3fb20eea2244a182a89cfd04b8c52c2d0d33a01b
Norihiro Kumagai <tendai22plus@gmail.com>
2024-05-23 15:48:29 +0900

    opcode.c: renum instruction code, minimize, 54 instructions

--- src/opcode.c
+++ src/opcode.c
@@ -77,13 +77,13 @@ undefined:
         fflush(stderr);
         return -1;
 
-    case 16: // m_dolit
+    case 9: // m_dolit
         w = STAR(cx->ip);
         cx->ip += CELLS;
         do_push(cx, w);
         cx->pc += CELLS;
         break;
-    case 17: // m_qbranch 
+    case 10: // m_qbranch 
         // conditional branch in thread
         if (do_pop(cx) != 0) {
             cx->ip += CELLS;       // skip branch operand
@@ -91,7 +91,7 @@ undefined:
             break;
         }
         // falling down
-    case 18: // m_branch 
+    case 11: // m_branch 
         // unconditional bra in thread
         addr = STAR(cx->ip);
         cx->ip = addr;
@@ -100,26 +100,26 @@ undefined:
     //
     // fetch and deposite (memory read/write)
     //
-    case 24: // m_bytedeposite
+    case 12: // m_bytedeposite
         w = do_pop(cx);
         mem[w] = do_pop(cx);
         if (STAR(DEBUG_ADDR))
             fprintf(stderr, "mem[%04x] = %04x\n", w, mem[w]);
         cx->pc += CELLS;
         break;
-    case 25: // m_exclamation
+    case 13: // m_exclamation
         w = do_pop(cx);
         STAR(w) = do_pop(cx);
         if (STAR(DEBUG_ADDR))
             fprintf(stderr, "mem[%04x] = %04x\n", w, STAR(w));
         cx->pc += CELLS;
         break;
-    case 26: // m_bytefetch
+    case 14: // m_bytefetch
         c = mem[tos(cx)];
         STAR(cx->sp) = c;
         cx->pc += CELLS;
         break;
-    case 27: // m_atfetch
+    case 15: // m_atfetch
         w = STAR(tos(cx));
         STAR(cx->sp) = w;
         cx->pc += CELLS;
@@ -127,7 +127,7 @@ undefined:
     //
     // stack operations
     //
-    case 32: // m_rot
+    case 16: // m_rot
         // (n1 n2 n3 -- n2 n3 n1)
         w = STAR(cx->sp + 2 * CELLS);
         STAR(cx->sp + 2 * CELLS) = STAR(cx->sp + CELLS);
@@ -135,22 +135,22 @@ undefined:
         STAR(cx->sp) = w;
         cx->pc += CELLS;
         break;
-    case 33: // m_swap
+    case 17: // m_swap
         w = STAR(cx->sp + CELLS);
         STAR(cx->sp + CELLS) = STAR(cx->sp);
         STAR(cx->sp) = w;
         cx->pc += CELLS;
         break;
-    case 34: // m_drop
+    case 18: // m_drop
         do_pop(cx);
         cx->pc += CELLS;
         break;
-    case 35: // m_over
+    case 19: // m_over
         w = STAR(cx->sp + 2);
         do_push(cx, w);
         cx->pc += CELLS;
         break;
-    case 36: // m_dup
+    case 20: // m_dup
         w = tos(cx);
         do_push(cx, w);
         cx->pc += CELLS;
@@ -158,26 +158,26 @@ undefined:
     //
     // return stack operations
     //
-    case 37:    // m_rsp
+    case 21:    // m_rsp
         do_push(cx, cx->rs);
         cx->pc += CELLS;
         break;
-    case 38:    // m_r2s
+    case 22:    // m_r2s
         w = do_popr(cx);
         do_push(cx, w);
         cx->pc += CELLS;
         break;
-    case 39: // m_s2r
+    case 23: // m_s2r
         w = do_pop(cx);
         do_pushr(cx, w);
         cx->pc += CELLS;
         break;
-    case 40: // m_r22s
+    case 24: // m_r22s
         w = STAR(cx->rs + CELLS);
         do_push(cx, w);
         cx->pc += CELLS;
         break;
-    case 41: // m_s2r2
+    case 25: // m_s2r2
         w = do_pop(cx);
         STAR(cx->rs + CELLS) = w;
         cx->pc += CELLS;
@@ -185,52 +185,52 @@ undefined:
     //
     // operators
     //
-    case 48: // m_not
+    case 26: // m_not
         STAR(cx->sp) = tos(cx) ? 0 : -1;
         cx->pc += CELLS;
         break;
-    case 49: // m_xor
+    case 27: // m_xor
         w = do_pop(cx);
         STAR(cx->sp) = tos(cx) ^ w;
         cx->pc += CELLS;
         break;
-    case 50: // m_or
+    case 28: // m_or
         w = do_pop(cx);
         STAR(cx->sp) = tos(cx) | w;
         cx->pc += CELLS;
         break;
-    case 51: // m_and
+    case 29: // m_and
         w = do_pop(cx);
         STAR(cx->sp) = tos(cx) & w;
         cx->pc += CELLS;
         break;
-    case 52: // m_div
+    case 30: // m_div
         w = do_pop(cx);
         STAR(cx->sp) = tos(cx) / w;
         cx->pc += CELLS;
         break;
-    case 53: // m_mul
+    case 31: // m_mul
         w = do_pop(cx);
         STAR(cx->sp) = tos(cx) * w;
         cx->pc += CELLS;
         break;
-    case 54: // m_sub
+    case 32: // m_sub
         w = do_pop(cx);
         STAR(cx->sp) = tos(cx) - w;
         cx->pc += CELLS;
         break;
-    case 55: // m_add
+    case 33: // m_add
         w = do_pop(cx);
         STAR(cx->sp) = tos(cx) + w;
         cx->pc += CELLS;
         break;
-    case 56: // m_gt
+    case 34: // m_gt
         w2 = do_pop(cx);
         w = tos(cx);
         tos(cx) = ((sword_t)w > (sword_t)w2) ? -1 : 0 ;
         cx->pc += CELLS;
         break;
-    case 57: // m_slashmod
+    case 35: // m_slashmod
         // fprintf(stderr, "%04x %d %d /mod ============================\n", cx->ip, w2, w);
         w = do_pop(cx);     // n2
         w2 = do_pop(cx);    // n1
@@ -248,7 +248,7 @@ undefined:
         cx->pc += CELLS;
         break;
 #endif //UNUSED_PRIMITIVES
-    case 65: // m_emit
+    case 36: // m_emit
         // put one byte to screen
         w = do_pop(cx);
         do_emit(cx, w);
@@ -310,15 +310,15 @@ undefined:
         cx->pc += CELLS;
         break;
 #endif //UNUSED_PRIMITIVES
-    case 77: // m_start_colondef
+    case 37: // m_start_colondef
         do_start_colondef(cx);
         cx->pc += CELLS;
         break;
-    case 78: // m_end_colondef
+    case 38: // m_end_colondef
         do_end_colondef(cx);
         cx->pc += CELLS;
         break;
-    case 79: // m_dictdump
+    case 39: // m_dictdump
         dump_entry(cx);
         cx->pc += CELLS;
         break;
@@ -343,7 +343,7 @@ undefined:
         cx->pc += CELLS;
         break;
 #endif //UNUSED_PRIMITIVES
-    case 84: // m_add_rsp
+    case 40: // m_add_rsp
         cx->rs += do_pop(cx);
         cx->pc += CELLS;
         break;
@@ -366,15 +366,15 @@ undefined:
         cx->pc += CELLS;
         break;
 #endif //UNUSED_PRIMITIVES
-    case 89: // m_sp_at
+    case 41: // m_sp_at
         do_push(cx, cx->sp);
         cx->pc += CELLS;
         break;
-    case 90: // m_lnum
+    case 42: // m_lnum
         do_lnum(cx);
         cx->pc += CELLS;
         break;
-    case 98: // m_getline
+    case 43: // m_getline
         // ( n addr -- -1 (ready)|0 (eof) )
         w = do_pop(cx);
         n = do_pop(cx);
@@ -383,25 +383,25 @@ undefined:
         do_push(cx, result);
         cx->pc += CELLS;
         break;
-    case 99: // m_outer
+    case 44: // m_outer
         do_push(cx, outer_flag);
         cx->pc += CELLS;
         break;
-    case 100:   // m_dadd
+    case 45:   // m_dadd
         // D+, double add
         ud1 = do_dpop(cx);
         ud2 = do_dpop(cx);
         do_dpush(cx, ud1 + ud2);
         cx->pc += CELLS;
         break;
-    case 101:  // m_madd
+    case 46:  // m_madd
         // ( d n --- d(=d+n) )
         w = do_pop(cx);
         ud1 = do_dpop(cx) + w;
         do_dpush(cx, ud1);
         cx->pc += CELLS;
         break;
-    case 102:  // m_mmuldiv
+    case 47:  // m_mmuldiv
         // ( d1 n2 n3 --- t-result )
         w2 = do_pop(cx);
         w = do_pop(cx);
@@ -411,29 +411,29 @@ undefined:
         do_dpush(cx, ud1);
         cx->pc += CELLS;
         break;
-    case 103:   // m_dlt
+    case 48:   // m_dlt
         // ( d1 d2 --- d(-1 if d1<d2, 0 if d1>=d2) )
         ud2 = do_dpop(cx);
         ud1 = do_dpop(cx);
         do_push(cx, (ud1 < ud2) ? (word_t)(-1) : 0);
         cx->pc += CELLS;
         break;
-    case 104: // m_umul
+    case 49: // m_umul
         d1 = do_pop(cx);
         d1 *= (uint32_t)do_pop(cx);
         do_push(cx, d1&0xffff);
         do_push(cx, (d1>>16)&0xffff);
         cx->pc += CELLS;
         break;
-    case 105: // m_rsp_reset
+    case 50: // m_rsp_reset
         cx->rs = RSTACK_END;
         cx->pc += CELLS;
         break;
-    case 106: // m_sp_reset
+    case 51: // m_sp_reset
         cx->sp = DSTACK_END;
         cx->pc += CELLS;
         break;
-    case 107: // m_s_dolit
+    case 52: // m_s_dolit
         do_push(cx, cx->ip);
         n = mem[cx->ip];        // c-string length
         n = (n + 1 + CELLS - 1) / CELLS * CELLS;
@@ -463,7 +463,7 @@ undefined:
         cx->pc += CELLS;
         break;
 #endif //UNUSED_PRIMITIVES
-    case 109: // m_d_dolit
+    case 53: // m_d_dolit
         w = STAR(cx->ip);
         cx->ip += CELLS;
         do_push(cx, w);
