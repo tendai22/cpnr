8d2c6b157f4dcc18277c1ee2521757b2f2580814
Norihiro Kumagai <tendai22plus@gmail.com>
2024-06-20 09:29:03 +0900

    cold.f: slight change

--- src/cold.f
+++ src/cold.f
@@ -2,6 +2,6 @@
 \ cold.f ... cold start routines
 \
 \ cmove ( from to count --- )
-: init_dump DP_HEAD dup @ END_ADDR DP_ADDR - ( 0x41 .ps ) cmove ;
+: init_dump DP_HEAD DP_ADDR END_ADDR DP_ADDR - 0x41 .ps cmove ;
 : cold init_dump abort ;
-\ ' cold COLD_HEAD !
+' cold COLD_HEAD !
