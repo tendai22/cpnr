edf4e553a0752ae5ff7e1fbb719abae4a9d02cad
Norihiro Kumagai <tendai22plus@gmail.com>
2024-08-05 01:10:47 +0900

    asciiart.f: some fixes are applied

--- src/asciiart.f
+++ src/asciiart.f
@@ -5,19 +5,11 @@
 \ asciiart.f: delived from C code on 
 \    `https://github.com/tgtakaoka/retro-bionic/tree/main`
 \
-variable f 
-variable x
-variable y
-variable a
-variable b
-variable c
-variable d
-variable ii
-variable p
-variable q
-variable s
-variable t
-variable tmp
+variable f variable x variable y
+variable a variable b variable c
+variable d variable ii variable p
+variable q variable s variable t
+variable tmp 
 variable eol  0 eol !
 variable out
 
@@ -25,8 +17,8 @@ variable out
     50 f !
     13 -12 do i y !
         30 -49 do i x !
-        x @ s->d 229 100 m*/ drop c ( 0x55 .ps ) !
-        y @ s->d 416 100 m*/ drop d ( 0x56 .ps ) !
+        x @ 229 * 100 / c ( 0x55 .ps ) !
+        y @ 416 * 100 / d ( 0x56 .ps ) !
         c @ a !
         d @ b !
         0 ii !
